5870b6110526334cbd0677a4bc42bd8d9e68e040
PouriaDan <pouriadan@gmail.com>
2016-05-29 16:01:01 +0430

    Update views.py

--- ShelegramWeb/Shelegram/views.py
+++ ShelegramWeb/Shelegram/views.py
@@ -6,7 +6,7 @@ from django.views.generic import View, ListView
 from Shelegram.forms import RegistrationForm;
 from Shelegram.models import ShelegramUser
 from django.contrib.auth import logout
-
+from Shelegram.models import ShelegramUser, ShelegramGroup
 
 def index(request):
     return render(request, 'shelegram/index.html', {})
@@ -41,6 +41,36 @@ class Register(View):
                                   {'user_model_register_form': user_model_register_form,
                                    'registered': registered},
                                   context)
+                                  
+class CreateGroup(View):
+    group_creation_form = GroupCreationForm()
+    template_name = 'shelegram/createGroup.html'
+
+    def get(self, request, *args, **kwargs):
+        context = RequestContext(request)
+        return render_to_response(self.template_name,
+                                  {'group_creation_form': self.group_creation_form,
+                                   'created': False},
+                                  context)
+
+    def post(self, request, *args, **kwargs):
+        created = False
+        context = RequestContext(request)
+        group_creation_form = GroupCreationForm(data=request.POST)
+        if group_creation_form.is_valid():
+            user = ShelegramUser.objects.get(pk=request.user.pk)
+            group = group_creation_form.save()
+            group.admin = user
+            if 'picture' in request.FILES:
+                group.picture = request.FILES['picture']
+            group.save()
+            created = True
+        return render_to_response(self.template_name,
+                                  {'group_creation_form': group_creation_form,
+                                   'created': created},
+                                  context)
+                                  
+                                  
 def profile(request):
     try:
         logged_in = ShelegramUser.objects.get(username = request.user)
