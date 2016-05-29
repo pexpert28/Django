f5c47dadb41f5fa4e6b92d46a967c68b8b2733aa
PouriaDan <pouriadan@gmail.com>
2016-05-29 16:00:00 +0430

    Update models.py

--- ShelegramWeb/Shelegram/models.py
+++ ShelegramWeb/Shelegram/models.py
@@ -7,4 +7,15 @@ class ShelegramUser(User):
     picture = models.ImageField(upload_to='/static/media/images/avatars/', default='/static/media/images/avatars/default.png', blank=True)
     User._meta.get_field('email').blank = False;
     def __str__(self):
-        return self.username;
+        return self.username
+
+        
+class ShelegramGroup(models.Model):
+    name = models.CharField(max_length=100, blank=False);
+    admin = models.ForeignKey(ShelegramUser, on_delete=models.CASCADE, null=False, related_name='admin_user')
+    picture = models.ImageField(upload_to='static/avatars/', default='static/avatars/default.jpg', blank=True)
+
+    def __str__(self):
+        return self.name
+        
+        
